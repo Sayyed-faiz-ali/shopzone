538e5a12c6c1ced3dec93a90d89e84bcf3048b8b
Faiz Ali <fa3838793@gmail.com>
2025-01-25 14:16:04 +0530

    new

--- config/mongoose-connections.js
+++ config/mongoose-connections.js
@@ -1,11 +1,21 @@
-const mongoose = require("mongoose");
+require('dotenv').config(); 
 const config = require("config");
 const debug = require("debug")("development:mongoose");
+const mongoose = require("mongoose");
 
-mongoose.connect(`${config.get('mongodb_URI')}/usermodel`)
-    .then(function() {
-        debug("connected");
+const mongoURI = config.has('mongodb_URI') 
+    ? config.get('mongodb_URI') 
+    : process.env.mongodb_URI;
+
+if (!mongoURI) {
+    console.error("Error: MongoDB URI is not defined in config or environment variables.");
+    process.exit(1); 
+}
+
+mongoose.connect(`${mongoURI}/usermodel`)
+    .then(() => {
+        debug("Connected to MongoDB.");
     })
-    .catch(function(err) {
+    .catch((err) => {
         console.error("Connection error:", err);
     });
