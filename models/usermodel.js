4e724500a786e18294afd9e54fae4e5bc9e12bb1
Sayyed-faiz-ali <156159566+Sayyed-faiz-ali@users.noreply.github.com>
2025-01-30 19:31:09 +0530

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,32 +1,18 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    fullname: { type: String, 
-        required: true 
-    },
-    email: { type: String, 
-        required: true 
-    
-     },
-    password: { type: String, required: true },
-    cart: [
-        {
-            type: mongoose.Schema.Types.ObjectId,ref: "product",
-            quantity: { type: Number, default: 1 }
-        },
-    ],
-  
-
-    orders: {
-        type: Array,
-        default: [],
-    },
-  
-    contactno: { type: String },
-    Picture: {
-        type:Buffer,
-        default:"total.png",
-        },
+const CartItemSchema = new mongoose.Schema({
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
+    quantity: { type: Number, default: 1 }
 });
 
-module.exports = mongoose.model("user", userSchema);
+const userSchema = new mongoose.Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    cart: [CartItemSchema], 
+    orders: { type: Array, default: [] },
+    contactno: { type: String },
+    Picture: { type: Buffer, default: "total.png" }
+});
+
+module.exports = mongoose.model("User", userSchema);
